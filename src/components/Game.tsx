f01132b1cedbc6b35a7953b90231d5de9fa00288
Julien Lebron <lebron-pro@outlook.fr>
2025-07-19 14:28:59 +0200

    ui thread

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -9,10 +9,16 @@ import Animated, {
   withSequence,
   withRepeat,
   withDelay,
+  runOnJS,
 } from "react-native-reanimated";
 
 export default function Game() {
   const x = useSharedValue(200);
+
+  const onReturned = () => {
+    "worklet";
+    console.log("returned to initial position");
+  };
 
   const moveBall = () => {
     // x.value = withTiming(x.value + 100, { duration: 5000 });
@@ -21,16 +27,26 @@ export default function Game() {
 
     // shake the ball
     x.value = withRepeat(
-      withSequence(withTiming(230), withTiming(170), withTiming(200)),
+      withSequence(
+        withTiming(230),
+        withTiming(170),
+        withTiming(200, {}, () => {
+          onReturned();
+          // runOnJS(onReturned)();
+        })
+      ),
       -1
     );
   };
 
   const ballStyles = useAnimatedStyle(() => {
+    // running on the UI
     return {
       left: x.value,
     };
   });
+
+  console.log("rerender");
 
   return (
     <SafeAreaView style={styles.container}>
